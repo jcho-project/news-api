2d43b9a295dbfd0cc030ca58f3d109804dcb084a
June Hyok Cho <jcho0627@gmail.com>
2019-04-05 11:34:41 +0200

    api connection check complete

--- news-api/app.js
+++ news-api/app.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 const request = require("request");
 
-app.use("view enging", "ejs");
+app.set("view enging", "ejs");
 
+app.get("/", (req, res) => {
+  let url = "https://newsapi.org/v2/everything?q=lg&from=2019-04-04&to=2019-04-04&sortBy=popularity&apiKey=0444a705c51c45ad8ef8e13241bf99a4";
 
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let data = JSON.parse(body);
+
+      res.send(data);
+    }
+  });
+});
 
 app.listen(4000, () => "Server is Listening on Port : 4000");
