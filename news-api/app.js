0f9dfc9ef86cb7b5b5dd73739c7f309e388b2989
June Hyok Cho <jcho0627@gmail.com>
2019-04-09 17:32:46 +0200

    top-headline.ejs initial and update

--- news-api/app.js
+++ news-api/app.js
@@ -11,16 +11,25 @@ app.get("/", (req, res) => {
 
 app.get("/search", (req, res) => {
   let query = req.query.search;
-  // Search all articles
-  // let url = "https://newsapi.org/v2/everything/?q=" + query + "&language=en&apiKey=0444a705c51c45ad8ef8e13241bf99a4";
-  // Search top headlines
-  let url = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=0444a705c51c45ad8ef8e13241bf99a4";
+  let url = "https://newsapi.org/v2/everything/?q=" + query + "&language=en&apiKey=0444a705c51c45ad8ef8e13241bf99a4";
 
   request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       let data = JSON.parse(body);
 
       res.render("search", { data: data });
+    }
+  });
+});
+
+app.get("/top-headlines", (req, res) => {
+  let url = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=0444a705c51c45ad8ef8e13241bf99a4";
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      let data = JSON.parse(body);
+
+      res.render("top-headlines", { data: data });
     }
   });
 });
