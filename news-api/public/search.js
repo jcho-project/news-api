b834691155f6647d7f02767d35dba0fbc9ec8099
June Hyok Cho <jcho0627@gmail.com>
2019-06-25 12:51:16 +0200

    search feature with id added

--- news-api/public/search.js
+++ news-api/public/search.js
@@ -1,1 +1,20 @@
-console.log("I have been loaded!");
+let cards = document.querySelectorAll("#source-card");
+
+let input = document.querySelector(".search-text");
+let sourceNames = document.querySelectorAll(".source-name");
+
+input.addEventListener("keyup", search);
+
+function search() {
+  let searchTerm = input.value.toUpperCase();
+
+  for (let i = 0; i < cards.length; i++) {
+    if (sourceNames[i].textContent.toUpperCase().indexOf(searchTerm) > -1) {
+      // cards[i].classList.add("display");
+      cards[i].style.display = "";
+    } else {
+      // cards[i].classList.remove("display");
+      cards[i].style.display = "none";
+    }
+  }
+}
